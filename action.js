13edb8662a9a29d7fcaace8d5fee160a5541de6a
dn4lele <danielmaier4312@gmail.com>
2023-04-18 09:20:32 +0300

    Pokémon

--- action.js
+++ action.js
@@ -1,10 +1,39 @@
 import express from "express";
 const router =express.Router();
+import fetch from "node-fetch";
 
 
 router.get("/getDataFromApi",async(req,res)=>{
+
+    const responseFromServer = await fetch(
+        "https://api.pokemontcg.io/v2/cards",
+        {method:'get'}
+    );
+
+    const fromatdata=await responseFromServer.json();
+
+
+
     return res.status(200).json({
-        data:"hello from api"
+        data:fromatdata
+    })
+})
+
+
+
+router.get("/getPokeById/:pokeId",async(req,res)=>{
+
+    const responseFromServer = await fetch(
+        `https://api.pokemontcg.io/v2/cards/${req.params.pokeId}`,
+        {method:'get'}
+    );
+
+    const fromatdata=await responseFromServer.json();
+
+
+
+    return res.status(200).json({
+        data:fromatdata
     })
 })
 
