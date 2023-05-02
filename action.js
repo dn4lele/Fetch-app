370d0db69cb4a83e1527de37fcbf5b46784eb38c
dn4lele <danielmaier4312@gmail.com>
2023-05-02 10:07:39 +0300

    corona today
    new corona data today

--- action.js
+++ action.js
@@ -6,9 +6,7 @@ import fetch from "node-fetch";
 
 router.get('/Ejs', async(req,res)=>{
 
-    res.render('index',{
-        myname:'daniel'
-    }) 
+    res.render('index',{myname:'daniel'}) 
 
 })
 
@@ -23,11 +21,15 @@ router.get("/getAllPoke",async(req,res)=>{
 
     const fromatdata=await responseFromServer.json();
 
-
-
+/*
+    return res.status(200).json({
+        data:fromatdata
+    })
+      */
     res.render("pokeindex", {
         allCards: fromatdata.data,
     });
+  
 })
 
 
@@ -41,11 +43,11 @@ router.get("/getPokeById/:pokeId",async(req,res)=>{
 
     const fromatdata=await responseFromServer.json();
  
-    /*
+     /*
     return res.status(200).json({
         data:fromatdata
     })
-     */
+    */
     res.render("pokedetail", {
         pokemon: fromatdata.data,
     });
@@ -58,7 +60,7 @@ router.get("/getPokeByname/:pokename",async(req,res)=>{
         {method:'get'}
         );
     const allCards = await responseFromServer.json();
-    const filteredCards = allCards.data.filter((card) => card.name.toLowerCase() === req.params.pokename.toLowerCase());
+    const filteredCards = allCards.data.filter((card) => card.name.toLowerCase() == req.params.pokename.toLowerCase());
 
 
 
@@ -67,6 +69,65 @@ router.get("/getPokeByname/:pokename",async(req,res)=>{
     return res.status(200).json({
         message:filteredCards
     })
+
+
+})
+
+
+router.get("/coronatoday",async(req,res)=>{
+    const responseFromServer = await fetch(
+        `https://api.covidtracking.com/v1/states/daily.json`,
+        {method:'get'}
+        );
+    const formatdata = await responseFromServer.json();
+
+    //by death
+    const filteredData = formatdata.filter(entry => entry.death != null);//remove all the states with no death
+    const sortedData = filteredData.sort((a, b) => b.death - a.death);//sort them by death
+
+    const uniqueStates = [];
+    const stateSet = new Set();
+
+    for (const obj of sortedData) {
+        if (!stateSet.has(obj.state)) {
+            stateSet.add(obj.state);
+            uniqueStates.push(obj);
+        }
+    }
+
+
+    let firstTen = uniqueStates.slice(0, 10);
+
+    const newData = firstTen.map(item => ({state: item.state ,death: item.death  }));
+
+
+    //same thing but with posivite
+    const filteredPositiveData = formatdata.filter(entry => entry.positive != null); // remove all the states with no positive cases
+    const sortedPositiveData = filteredPositiveData.sort((a, b) => b.positive - a.positive); // sort them by positive cases
+
+    const uniquePositiveStates = [];
+    const positiveStateSet = new Set();
+
+    for (const obj of sortedPositiveData) {
+    if (!positiveStateSet.has(obj.state)) {
+        positiveStateSet.add(obj.state);
+        uniquePositiveStates.push(obj);
+    }
+    }
+
+    let firstTenPositiveStates = uniquePositiveStates.slice(0, 10);
+
+    const newPositiveData = firstTenPositiveStates.map(item => ({state: item.state, positive: item.positive}));
+
+    
+      return res.status(200).json({
+        by_death:newData,
+        by_positive:newPositiveData
+    })
+    res.render("coronadata", {
+        alldata: top10CountriesByDeaths,
+    });
+   
 
 
 })
